f45a46d0659beb6f6ed5446d2aa9e4af8c574068
Alex Palmer <alexpalmer37@gmail.com>
2021-11-01 22:06:19 -0400

    deploying

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
